c745d5ab4643ea1eb4ca4e548026467590a7727d
Lev Ustimenko <Leva3175@gmail.com>
2023-12-07 19:19:23 +0300

    init

---
+++ src/main/java/com/levandr/lev/LevSpringApplication.java
@@ -0,0 +1,13 @@
+package com.levandr.lev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LevSpringApplication.class, args);
+	}
+
+}
