946f244cedf08a6053ec32b65f739d77ced59aa0
Lev Ustimenko <Leva3175@gmail.com>
2023-12-07 18:02:44 +0300

    init commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import org.springframework.context.ApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ApplicationContext applicationContext;
+
     }
 }
